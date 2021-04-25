645ccb616a19a209dab527c2a44e59bd87a66d14
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2021-04-25 13:34:17 -0300

    :recycle: only allow to fire to up (not fire to the floor)

--- challenges/clase-3/entities/player/Cannon.gd
+++ challenges/clase-3/entities/player/Cannon.gd
@@ -3,6 +3,8 @@ extends Sprite
 export (PackedScene) var projectile_scene: PackedScene
 
 var _container: Node
+const LEFT_BOUND_ANGLE: float = -PI + 0.15
+const RIGHT_BOUND_ANGLE: float = 0 - 0.15
 
 onready var fire_position = $FirePosition
 
@@ -17,4 +19,16 @@ func fire() -> void:
 	new_projectile.initialize(_container, from_position, to_position)
 
 func point_to(position: Vector2) -> void:
-	rotation = position.normalized().angle()
+	var new_angle = position.normalized().angle()
+
+	# Lower right quadrant
+	if new_angle < PI / 2 && new_angle > RIGHT_BOUND_ANGLE:
+		rotation = RIGHT_BOUND_ANGLE
+		return
+	
+	# Lower left quadrant
+	if new_angle >= PI / 2 || new_angle < LEFT_BOUND_ANGLE:
+		rotation = LEFT_BOUND_ANGLE
+		return
+	
+	rotation = new_angle
