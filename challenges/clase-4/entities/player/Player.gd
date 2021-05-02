bf8958852ae4cfd84a32441bb65dbc480d2abebb
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2021-05-02 17:24:05 -0300

    :recycle: add tilemap

--- challenges/clase-4/entities/player/Player.gd
+++ challenges/clase-4/entities/player/Player.gd
@@ -6,9 +6,9 @@ export (float) var ACCELERATION:float = 20.0
 export (float) var H_SPEED_LIMIT:float = 600.0
 export (float) var FRICTION_WEIGHT:float = 0.1
 export (float) var GRAVITY: float = 10.0
-export (float) var JUMP_VELOCITY: float = 500.0
+export (float) var JUMP_VELOCITY: float = 400.0
 
-var velocity:Vector2 = Vector2.ZERO
+var velocity: Vector2 = Vector2.ZERO
 var projectile_container
 
 func initialize(projectile_container):
@@ -16,9 +16,8 @@ func initialize(projectile_container):
 	cannon.projectile_container = projectile_container
 
 func _physics_process(delta):
-	
 	# Cannon rotation
-	var mouse_position:Vector2 = get_global_mouse_position()
+	var mouse_position: Vector2 = get_global_mouse_position()
 	cannon.look_at(mouse_position)
 	
 	# Cannon fire
@@ -36,13 +35,15 @@ func _physics_process(delta):
 	else:
 		velocity.x = lerp(velocity.x, 0, FRICTION_WEIGHT) if abs(velocity.x) > 1 else 0
 	
-	move_and_slide(Vector2.DOWN, -Vector2.DOWN) # hackazo para el is_on_floor()
+#	move_and_slide(Vector2.DOWN, -Vector2.DOWN) # hackazo para el is_on_floor()
+	if is_on_floor():
+		velocity.y = 0
 	if is_on_floor() and Input.is_action_pressed("jump"):
 		velocity.y -= JUMP_VELOCITY
-	else:
-		velocity.y += GRAVITY
 	
-	move_and_slide(velocity)
+	velocity.y += GRAVITY
+	print(velocity.y)
+	move_and_slide(velocity, Vector2.UP)
 
 func notify_hit():
 	print("ouch")
